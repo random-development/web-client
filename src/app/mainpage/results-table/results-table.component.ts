515536a4ef3347b991e3e9a9903122a2f95088e7
Szymon Miazga <szymek26@outlook.com>
2019-04-28 22:55:09 +0200

    Chart window and table fixes

--- src/app/mainpage/results-table/results-table.component.ts
+++ src/app/mainpage/results-table/results-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MetricValues } from '../metrics/metric-values';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-results-table',
@@ -8,10 +10,38 @@ import { MetricValues } from '../metrics/metric-values';
   styleUrls: ['./results-table.component.scss']
 })
 export class ResultsTableComponent {
-
+  closeResult: string;
+  modalContent: undefined;
+  public chartType: string = 'line';
   @Input()
   metrics: MetricValues[];
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
+  }
+
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = [];
+  public barChartType = 'line';
+  public barChartLegend = true;
+  public barChartData = [];
+  public timestampsConverted = [];
+
+  open(content, metric) {
+    this.modalContent = metric;
+    metric.timeData.forEach(element => {
+      this.timestampsConverted.push(new Date(element*1000).toLocaleTimeString([], {year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute:'2-digit', second: '2-digit'}));
+    });
+    this.barChartData = [
+      {data: metric.valueData, label: 'Value'}
+    ];
+    this.barChartLabels = this.timestampsConverted;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed`;
+    });
   }
 }
--- src/app/mainpage/results-table/results-table.module.ts
+++ src/app/mainpage/results-table/results-table.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultsTableComponent } from './results-table.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     ResultsTableComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ChartsModule
   ],
   exports: [
     ResultsTableComponent
