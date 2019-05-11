6c5d5717afda516f80abb500e38c84fa53893a53
Mikolaj Szczepanski <mikolaj.szczepanski@outlook.com>
2019-05-11 19:14:10 +0200

    Add new configuration for only CI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://hibron.usermd.net:5000/mock'
-  baseUrl: 'http://hibron.usermd.net:5000/gateway'
+  baseUrl: 'http://localhost:5000'
 };
